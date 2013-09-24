4987089f0cf326b6c8d9d1cd912b7e6aa383e134
Riccardo Causo <riccardo.causo@PWS0000461.lugano.ch>
2013-09-24 10:59:58 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StatsExpose::Application.config.secret_key_base = '7432de5912d17c10787f012bb1cd3bcfb875062afb358165adbe8ca0e985b7512a7d3035b3d2171aadd3eea49099e4d16cdfcf4741a0a5bb1c6faf11dc2e3ff6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StatsExpose::Application.config.session_store :cookie_store, key: '_StatsExpose_session'
